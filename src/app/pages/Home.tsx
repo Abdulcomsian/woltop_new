34c397b1195052ccd1ff86d456bd25a90f894b11
naccrual <nauman.riaz@accrualhub.com>
2025-01-08 11:23:01 +0500

    shipping page changes

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -24,6 +24,7 @@ import {
 } from "~/store/api/productApi";
 import { useGetColorsQuery, useGetTagsQuery } from "~/store/api/paramApi";
 import TabsComponent from "~/components/tabComponent";
+import SwiperCard from "~/components/swiperCard";
 export default function Home() {
   const {
     data: popularProducts,
@@ -91,7 +92,7 @@ export default function Home() {
   // ];
   return (
     <main>
-     
+
 
       <TopBar time={{ hours: 0, minutes: 7, seconds: 27 }}></TopBar>
       <Navbar></Navbar>
