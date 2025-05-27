e1d80d8617e5a43b7f0a12312d5ebfb86536b1f2
Abdulcomsian <basitawn.abdul@gmail.com>
2025-05-27 19:12:20 +0500

    fix: update postcss for purge

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,33 @@
 export default {
   plugins: {
     tailwindcss: {},
+    ...(process.env.NODE_ENV === "production"
+      ? {
+          "@fullhuman/postcss-purgecss": {
+            content: [
+              "./pages/**/*.{js,jsx,ts,tsx}",
+              "./components/**/*.{js,jsx,ts,tsx}",
+              "./app/**/*.{js,jsx,ts,tsx}",
+              "./src/**/*.{js,jsx,ts,tsx}",
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+            safelist: {
+              standard: ["html", "body"],
+              deep: [
+                /^dark:/,
+                /^[a-z]+:/,
+                /^peer-/,
+                /^group-/,
+                /^data-/,
+                /^aria-/,
+                /^rtl:/,
+                /^ltr:/,
+              ],
+              greedy: [/^Toastify/, /^slick-/, /^react-/, /^swiper/],
+            },
+          },
+        }
+      : {}),
   },
 };
